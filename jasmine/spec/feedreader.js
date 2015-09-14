f0c46d9301af238d4f3d760b703cff2208cf9c01
Mark Anderson <udacity.com@ma7.org>
2015-09-14 17:59:27 -0500

    13. Wte a test suite named 'Initial Entries'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,7 +55,7 @@ $(function() {
           */
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -63,6 +63,7 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
